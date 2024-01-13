b7357af3e8b63de01f14c22d6e8742d39e93b3a4
Matthew Deglinnocenti <info@jackiedeglinnocenti.com>
2024-01-13 23:25:47 +0100

    Add functionalities for note and zip, improved Readme

--- Readme.md
+++ Readme.md
@@ -3,3 +3,18 @@
 #### A full commands suite in Perl for your command line interface
 
 *2024-1-13 Under development*
+
+## Requirements
+
+#### Modules
+
+It's needed to install the following modules before running the script:
+
+- Switch
+- Cwd
+- JSON::XS
+- File::Slurp
+
+You can install it by this:
+
+cpan Switch, Cwd, Json::XS, File::Slurp
--- program.pl
+++ program.pl
@@ -2,18 +2,22 @@
 
 use Switch;
 use Cwd;
+
 #use warnings;
 
 use JSON::XS qw(encode_json decode_json);
 use File::Slurp qw(read_file write_file);
+use IO::Compress::Zip ();
+use IO::Uncompress::Unzip ();
 
-my $STORAGE = './storage';
+my $LINKS = './storagefile-links';
+my $NOTE = './storagefile-note';
 
 my $firstArgument = $ARGV[0];
 
 if (-e $firstArgument) {
     #handleFile($firstArgument);
-    return; 
+    exit; 
 }
 
 my $command = $firstArgument;
@@ -21,8 +25,13 @@ my $command = $firstArgument;
 switch( $command ) {
     case 'go' { go(); }
     case 'goset' { goset(); }
-    else {}
+    case 'note' { note(); }
+    case 'zip' { zip(); }
+    case 'unzip' { unzip(); }
+    else { print "Command not found\n"; }
 }
+
+####
 
 sub go() {
     my $where = $ARGV[1];
@@ -38,9 +47,50 @@ sub goset() {
     print "New path link saved.\n";
 }
 
+####
+
+sub note() {
+    print "\033[2J";    # clear the screen
+    print "\033[0;0H";  # jump to 0,0
+
+    if ( -e $NOTE ) {
+        my $text = read_file($NOTE, { binmode => ':raw' });
+        print $text;
+    }
+
+    my @a;
+    $a[0] = $text;
+    while (<STDIN>) {
+        /\S/ or last; # last line if empty
+        push @a, $_;
+    }
+
+    write_file($NOTE, { binmode => ':raw' }, join('', @a) );
+}
+
+sub zip() {
+    my $what = $ARGV[1];
+    if ( ! -e $what ) {
+        print "Can't compress: file not found\n";
+        exit;
+    }
+    IO::Compress::Zip::zip $what => "$what.zip" or die "zip failed: $ZipError\n";
+}
+
+sub unzip() {
+    my $what = $ARGV[1];
+    if ( ! -e $what ) {
+        print "Can't decompress: file not found\n";
+        exit;
+    }
+    IO::Uncompress::Unzip::unzip $what => substr($what, 0, -4) or die "unzip failed: $ZipError\n";
+}
+
+####
+
 sub getPathFromName() {
     my $name = shift;
-    my $json = read_file($STORAGE, { binmode => ':raw' });
+    my $json = read_file($LINKS, { binmode => ':raw' });
     my %hash = %{ decode_json($json) };
     return $hash{$name};
 }
@@ -49,11 +99,11 @@ sub setPathForName() {
     my $name = shift;
     my $where = shift;
 
-    my $json = read_file($STORAGE, { binmode => ':raw' });
+    my $json = read_file($LINKS, { binmode => ':raw' });
     my %hash = %{ decode_json($json) };
 
     $hash{$name} = $where;
     
     my $json = encode_json \%hash;
-    write_file($STORAGE, { binmode => ':raw' }, $json);
+    write_file($LINKS, { binmode => ':raw' }, $json);
 }
